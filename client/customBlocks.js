796e4090a2c76e3160228b3176a9f28e48ef92c4
kay <longguo0318@gmail.com>
2024-04-16 08:43:53 +0900

    client에 직접 데이터 넣음, 정상 동작

--- client/customBlocks.js
+++ client/customBlocks.js
@@ -138,12 +138,56 @@ function defineCustomBlocks(customBlocks) {
 }
 
 
-// 서버에서 블록 데이터를 비동기적으로 가져오는 함수
-async function fetchCustomBlocks() {
-  const response = await fetch('http://localhost:5000/api/blocks');
-  const customBlocks = await response.json();
+// // 서버에서 블록 데이터를 비동기적으로 가져오는 함수
+// async function fetchCustomBlocks() {
+//   const response = await fetch('http://localhost:5000/api/blocks');
+//   const customBlocks = await response.json();
+//   defineCustomBlocks(customBlocks);
+// }
+
+function fetchCustomBlocks() {
+  const customBlocks = [
+    {
+      "args1_name": "destination",
+      "args1_type": "str",
+      "args1_value": "",
+      "args2_name": "delay",
+      "args2_type": "int",
+      "args2_value": "0",
+      "args3_name": null,
+      "args3_type": null,
+      "args3_value": null,
+      "args4_name": null,
+      "args4_type": null,
+      "args4_value": null,
+      "args5_name": null,
+      "args5_type": null,
+      "args5_value": null,
+      "desc": "\ubaa9\uc801\uc9c0\ub97c \uc124\uc815\ud558\ub294 \ud568\uc218\r\ndestination: \uc124\uc815\ud558\ub824\ub294 \ubaa9\uc801\uc9c0\r\ndelay: \ud568\uc218 \uc2e4\ud589 \uc644\ub8cc \ud6c4 \ub51c\ub808\uc774 \uc2dc\uac04",
+      "func_name": "SetDestination"
+    },
+    {
+      "args1_name": "screenName",
+      "args1_type": "str",
+      "args1_value": "",
+      "args2_name": "delay",
+      "args2_type": "int",
+      "args2_value": "0",
+      "args3_name": null,
+      "args3_type": null,
+      "args3_value": null,
+      "args4_name": null,
+      "args4_type": null,
+      "args4_value": null,
+      "args5_name": null,
+      "args5_type": null,
+      "args5_value": null,
+      "desc": "\ud654\uba74 \uc774\ub3d9 \ud568\uc218",
+      "func_name": "GotoScreen"
+    }
+  ]
   defineCustomBlocks(customBlocks);
 }
 
-// 서버로부터 블록 데이터를 가져오는 함수 호출
-fetchCustomBlocks();
+// // 서버로부터 블록 데이터를 가져오는 함수 호출
+// fetchCustomBlocks();
--- client/returnedPython.js
+++ client/returnedPython.js
@@ -9,6 +9,8 @@ document.addEventListener("DOMContentLoaded", function() {
         scrollbars: false,
     });
     workspace.addChangeListener(updateCode); // 블록 변경 시 코드 업데이트
+
+    fetchCustomBlocks();
 });
 
 function updateCode(event) {
