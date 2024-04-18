2a8ad5078b4c7d130e6f9616a3d425a5df2d9d80
kay <longguo0318@gmail.com>
2024-04-18 16:02:50 +0900

    필요 없는 모듈 삭제

---
+++ client/event/customPopup.js
@@ -0,0 +1,2 @@
+// customPopup.js
+
--- client/tempBlocks.js
+++ client/tempBlocks.js
@@ -1,5 +1,6 @@
 const customBlocks = [
     {
+    "func_name": "SetDestination",
     "args1_name": "destination",
     "args1_type": "str",
     "args1_value": "",
@@ -15,10 +16,10 @@ const customBlocks = [
     "args5_name": null,
     "args5_type": null,
     "args5_value": null,
-    "desc": "\ubaa9\uc801\uc9c0\ub97c \uc124\uc815\ud558\ub294 \ud568\uc218\r\ndestination: \uc124\uc815\ud558\ub824\ub294 \ubaa9\uc801\uc9c0\r\ndelay: \ud568\uc218 \uc2e4\ud589 \uc644\ub8cc \ud6c4 \ub51c\ub808\uc774 \uc2dc\uac04",
-    "func_name": "SetDestination"
+    "desc": "\ubaa9\uc801\uc9c0\ub97c \uc124\uc815\ud558\ub294 \ud568\uc218\r\ndestination: \uc124\uc815\ud558\ub824\ub294 \ubaa9\uc801\uc9c0\r\ndelay: \ud568\uc218 \uc2e4\ud589 \uc644\ub8cc \ud6c4 \ub51c\ub808\uc774 \uc2dc\uac04"
     },
     {
+    "func_name": "GotoScreen",
     "args1_name": "screenName",
     "args1_type": "str",
     "args1_value": "",
@@ -34,7 +35,6 @@ const customBlocks = [
     "args5_name": null,
     "args5_type": null,
     "args5_value": null,
-    "desc": "\ud654\uba74 \uc774\ub3d9 \ud568\uc218",
-    "func_name": "GotoScreen"
+    "desc": "\ud654\uba74 \uc774\ub3d9 \ud568\uc218"
     }
 ]
