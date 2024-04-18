14d6391ed50844365d4ae330dd5470cb72a6312a
kay <longguo0318@gmail.com>
2024-04-18 14:06:23 +0900

    toolbox내 블록 정렬 기능 추가 - 정상 동작

---
+++ client/tempBlocks.js
@@ -0,0 +1,40 @@
+const customBlocks = [
+    {
+    "args1_name": "destination",
+    "args1_type": "str",
+    "args1_value": "",
+    "args2_name": "delay",
+    "args2_type": "int",
+    "args2_value": "0",
+    "args3_name": null,
+    "args3_type": null,
+    "args3_value": null,
+    "args4_name": null,
+    "args4_type": null,
+    "args4_value": null,
+    "args5_name": null,
+    "args5_type": null,
+    "args5_value": null,
+    "desc": "\ubaa9\uc801\uc9c0\ub97c \uc124\uc815\ud558\ub294 \ud568\uc218\r\ndestination: \uc124\uc815\ud558\ub824\ub294 \ubaa9\uc801\uc9c0\r\ndelay: \ud568\uc218 \uc2e4\ud589 \uc644\ub8cc \ud6c4 \ub51c\ub808\uc774 \uc2dc\uac04",
+    "func_name": "SetDestination"
+    },
+    {
+    "args1_name": "screenName",
+    "args1_type": "str",
+    "args1_value": "",
+    "args2_name": "delay",
+    "args2_type": "int",
+    "args2_value": "0",
+    "args3_name": null,
+    "args3_type": null,
+    "args3_value": null,
+    "args4_name": null,
+    "args4_type": null,
+    "args4_value": null,
+    "args5_name": null,
+    "args5_type": null,
+    "args5_value": null,
+    "desc": "\ud654\uba74 \uc774\ub3d9 \ud568\uc218",
+    "func_name": "GotoScreen"
+    }
+]
